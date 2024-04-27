b89d160e62bdc347a67205b1c04d8bc1c77399cf
kali <kali@kali.(none)>
2024-04-28 01:21:21 +0800

    settled 400 and 404 responses

--- includes/respond_builder.hpp
+++ includes/respond_builder.hpp
@@ -3,7 +3,7 @@
 #include <fstream> 
 #include "../includes/request_parser.hpp"
 
-class respond_get
+class respond_builder
 {
     private:
         request_data *request_info;
@@ -12,25 +12,39 @@ class respond_get
         std::string respond_body;
         std::string host_directory;
         int status;
+        std::string server;
+        std::string date;
+        std::string content_type;
+        int content_length;
+        std::string connection;
+
+// Server: nginx/1.24.0
+// Date: Sat, 27 Apr 2024 15:20:52 GMT
+// Content-Type: text/html
+// Content-Length: 153
+// Connection: keep-alive
+
 
     public:
-        respond_get(request_data *input, std::string host_directory);
+        respond_builder(request_data *input, std::string host_directory);
         void read_file();
+        void build_400_respond();
+        void build_404_respond();
         std::string build_respond_data();
 };
 
-class respond_post
-{
-    private:
-        request_data *request_info;
-        std::string respond_data;
-        std::string status_line;
-        std::string respond_body;
-        std::string host_directory;
-        int status;
+// class respond_post
+// {
+//     private:
+//         request_data *request_info;
+//         std::string respond_data;
+//         std::string status_line;
+//         std::string respond_body;
+//         std::string host_directory;
+//         int status;
 
-    public:
-        respond_post(request_data *input, std::string host_directory);
-        void read_file();
-        std::string build_respond_data();
-};
+//     public:
+//         respond_post(request_data *input, std::string host_directory);
+//         void read_file();
+//         std::string build_respond_data();
+// };
--- srcs/respond_builder.cpp
+++ srcs/respond_builder.cpp
@@ -1,17 +1,56 @@
 #include "../includes/respond_builder.hpp"
+#include <ctime>
 
-respond_get::respond_get(request_data *input, std::string host_directory) : request_info(input)
+respond_builder::respond_builder(request_data *input, std::string host_directory) : request_info(input)
 {
-    std::cout << "\nGenerating http respond..." << std::endl;
-    this->status = 200;
-    this->status_line = "HTTP/1.1 200 OK";
+    std::cout << "\n>>>>> Generating http respond <<<<<<" << std::endl;
+
+    this->server = "42_webserv";
+    this->connection = input->get_connection();
+
+    if (input->get_status_line() == 400)
+        this->build_400_respond();
+    else if (input->get_status_line() == 404)
+        this->build_404_respond();
+
+    // this->status = 200;
+    // this->status_line = "HTTP/1.1 200 OK";
     
-    // Body
-    this->host_directory = host_directory;
-    this->read_file();
+    // // Body
+    // this->host_directory = host_directory;
+    // this->read_file();
 }
 
-void respond_get::read_file()
+void respond_builder::build_400_respond()
+{
+    std::ifstream file;
+    std::ostringstream ss;
+
+    this->status = 400;
+    this->status_line = "HTTP/1.1 400 Bad Request";
+    this->content_type = "text/html";
+    file.open("../errors/400.html"); // need to path depending on where main is called!
+	ss << file.rdbuf();
+    this->respond_body = ss.str();
+    this->content_length = this->respond_body.length();
+}
+
+void respond_builder::build_404_respond()
+{
+    std::ifstream file;
+    std::ostringstream ss;
+
+    this->status = 404;
+    this->status_line = "HTTP/1.1 404 Not Found";
+    this->content_type = "text/html";
+    file.open("../errors/404.html"); // need to path depending on where main is called!
+	ss << file.rdbuf();
+    this->respond_body = ss.str();
+    this->content_length = this->respond_body.length();
+}
+
+
+void respond_builder::read_file()
 {
     std::ifstream file((host_directory + request_info->get_target()).c_str());
 
@@ -26,10 +65,25 @@ void respond_get::read_file()
     this->respond_body = ss.str();
 }
 
-std::string respond_get::build_respond_data()
+std::string respond_builder::build_respond_data()
 {
     std::string respond_txt;
-    respond_txt = this->status_line + "\r\n\r\n" + this->respond_body;
+    // get current time
+    std::time_t currentTime = std::time(NULL);
+    std::tm* localTime = std::gmtime(&currentTime); // Convert to GMT time
+    // Format the date string
+    char dateString[128];
+    std::strftime(dateString, sizeof(dateString), "%a, %d %b %Y %H:%M:%S GMT", localTime);
+
+    std::stringstream ss;
+    ss << this->content_length;
+    respond_txt = this->status_line + "\r\n"
+                    + "Server: " + this->server + "\r\n"
+                    + "Date: " + dateString + "\r\n"
+                    + "Content-Type: " + this->content_type + "\r\n"
+                    + "Content-Length: " + ss.str() + "\r\n"
+                    + "Connection: " + this->connection + "\r\n\r\n"
+                    + this->respond_body;
     this->respond_data = respond_txt;
     return(this->respond_data);
 }
@@ -56,18 +110,6 @@ int main()
     std::cout << input->get_body() << std::endl;
     std::cout << input->get_status_line() << std::endl;
 
-    if (input->get_status_line() != 200)
-    {
-        std::cout << "create error response" << std::endl;
-    }
-    else if (input->get_method() == "GET")
-    {
-        respond_get *output = new respond_get(input, host_directory);
-        std::cout << output->build_respond_data() << std::endl;
-    }
-    // else if (input->get_method() == "POST")
-    // {
-    //     respond_get *output = new respond_post(input, host_directory);
-    //     std::cout << output->build_respond_data() << std::endl;
-    // }
+    respond_builder *output = new respond_builder(input, host_directory);
+    std::cout << output->build_respond_data() << std::endl;
 }
